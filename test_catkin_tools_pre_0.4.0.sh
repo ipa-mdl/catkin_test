bd7ef1c40e84e81b31e2e62d713201a734ee9f02
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2016-02-04 00:40:36 +0100

    install dependencies

--- test_catkin_tools_pre_0.4.0.sh
+++ test_catkin_tools_pre_0.4.0.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -x
 
+sudo apt-get -y install python-pip 
 cd
 git clone https://github.com/catkin/catkin_tools.git -b pre-0.4.0-linked-devel
 cd catkin_tools
+sudo pip install -r requirements.txt
 python setup.py build
 sudo python setup.py install
 cd $CATKIN_WS
