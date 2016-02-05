e46ca8860edb482935e7933d90cc4ddf9ba60c5f
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2016-02-05 09:30:50 +0100

    Update test_catkin_tools_pre_0.4.0.sh

--- test_catkin_tools_pre_0.4.0.sh
+++ test_catkin_tools_pre_0.4.0.sh
@@ -3,8 +3,9 @@ set -x
 
 sudo apt-get -y install python-pip
 sudo pip install setuptools argparse catkin-pkg distribute PyYAML psutil trollius
-sudo pip install git+https://github.com/osrf/osrf_pycommon.gitcd
-git clone https://github.com/catkin/catkin_tools.git -b pre-0.4.0-linked-devel
+sudo pip install git+https://github.com/osrf/osrf_pycommon.git
+cd
+git clone https://github.com/catkin/catkin_tools.git
 cd catkin_tools
 python setup.py build
 sudo python setup.py install
